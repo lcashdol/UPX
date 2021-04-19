6ef46f6f34982461b2ac3b5ad93aeb1d55d7a77c
Larry W. Cashdollar <lcashdol@gmail.com>
2021-04-19 09:07:17 -0400

    updated to a dynamically allocated datastructure

--- upx_dec.c
+++ upx_dec.c
@@ -18,25 +18,33 @@ void print_usage (char *arg);
 int
 main (int argc, char **argv)
 {
-
-unsigned char data[2560000];
+struct stat stats;
+unsigned char *data;
 long int size=0;
 long int header=0;
-int total = 0, x = 0, head = 0, z, fd;
-FILE *ffile;
+int total = 0, x = 0, head = 0, z=0, fd=0, fout=0;
 char filename[256];
 
   if (argc < 2)
     print_usage (argv[0]);
 
-
   printf
-    ("UPX! Packed Binary un-corruptor v1.0\n\n\n4/15/2021 Larry Cashdollar - Akamai SIRT\n\n\nReading from file %s \n",
+    ("######################################################################################################\n# UPX! Packed Binary un-corruptor v1.0\n\n\n4/15/2021 Larry Cashdollar - Akamai SIRT\n\n\nReading from file %s \n",
      argv[1]);
+
+if (stat(argv[1], &stats) == 0)
+    {
+
+data = malloc(stats.st_size * sizeof(char));
+
+} else {
+      fprintf(stderr, "Error %s %s\n", argv[1], strerror( errno ));
+      exit (0);
+}
+
   fd = open (argv[1], O_RDONLY);
   if (fd < 0)
     {
-     // fprintf (stderr,"Error opening file %s\n Quitting. \n", argv[1]);
       fprintf(stderr, "Error %s %s\n", argv[1], strerror( errno ));
       exit (0);
     }
@@ -52,7 +60,7 @@ char filename[256];
 	  && data[x + 3] == 0x21)
 	{
 	  head++;
-	  printf ("Found UPX! Header Position %d\n", x);
+	  printf ("Found UPX! Header Position at %d ", x);
 
 	  if (head == 1)
 	    {
@@ -65,9 +73,11 @@ char filename[256];
 
 	    }
 
+          if (head == 2) printf("\n");
+
 	  if (head == 3)
 	    {
-	      printf ("UPX! p_filesize :");
+	      printf ("\nUPX! p_filesize :");
 
 	      size = x + 24;
 // position header 1 is 8 bytes
@@ -100,16 +110,26 @@ char filename[256];
     }
 
   snprintf (filename, 249, "%s.fixed", argv[1]);
-  ffile = fopen (filename, "wb");
+  fout = open (filename, O_WRONLY | O_CREAT |O_TRUNC,0644);
 
-  if (!ffile) {
+  if (!fout) {
       fprintf(stderr, "Error %s %s\n", filename, strerror( errno ));
       exit (0);
   }
 
   printf ("\nTotal bytes read %d\n\nWriting file %s\n", total, filename);
-  fwrite (&data, total, 1, ffile);
-  fclose (ffile);
+  total=0;
+  while (total < stats.st_size)
+    {
+      write (fout, &data[total], 1);
+      float percent=0.0, t=total,s=stats.st_size;
+      percent = (t/s) * 100;
+      if (total % 10000 == 0) printf("%.2f %%\n",percent);
+      total++;
+    }
+    printf("Done\n");
+  close (fout);
+  free(data);
   return 0;
 }
 
