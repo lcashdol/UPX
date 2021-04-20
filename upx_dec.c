156981c77b65fd23ed9c0d08c46c04ab20662324
Larry W. Cashdollar <larry0@me.com>
2021-04-20 11:31:43 -0400

    Update upx_dec.c

--- upx_dec.c
+++ upx_dec.c
@@ -10,42 +10,41 @@
 /*https://vcodispot.com/corrupted-upx-packed-elf-repair/*/
 /*https://cujo.com/upx-anti-unpacking-techniques-in-iot-malware/*/
 
-
 extern int errno;
 
 void print_usage (char *arg);
+void print_banner(char *arg);
 
 int
 main (int argc, char **argv)
 {
-struct stat stats;
-unsigned char *data;
-long int size=0;
-long int header=0;
-int total = 0, x = 0, head = 0, z=0, fd=0, fout=0;
-char filename[256];
+  struct stat stats;
+  unsigned char *data;
+  long int size = 0;
+  long int header = 0;
+  int total = 0, x = 0, head = 0, z = 0, fd = 0, fout = 0;
+  char filename[256];
 
   if (argc < 2)
     print_usage (argv[0]);
 
-  printf
-    ("######################################################################################################\n# UPX! Packed Binary un-corruptor v1.0\n\n\n4/15/2021 Larry Cashdollar\n\n\nReading from file %s \n",
-     argv[1]);
-
-if (stat(argv[1], &stats) == 0)
+   print_banner(argv[1]);
+  if (stat (argv[1], &stats) == 0)
     {
 
-data = malloc(stats.st_size * sizeof(char));
+      data = malloc (stats.st_size * sizeof (char));
 
-} else {
-      fprintf(stderr, "Error %s %s\n", argv[1], strerror( errno ));
+    }
+  else
+    {
+      fprintf (stderr, "Error %s %s\n", argv[1], strerror (errno));
       exit (0);
-}
+    }
 
   fd = open (argv[1], O_RDONLY);
   if (fd < 0)
     {
-      fprintf(stderr, "Error %s %s\n", argv[1], strerror( errno ));
+      fprintf (stderr, "Error %s %s\n", argv[1], strerror (errno));
       exit (0);
     }
   while (read (fd, &data[total], 1))
@@ -56,6 +55,14 @@ data = malloc(stats.st_size * sizeof(char));
 //search for UPX! headers then find p_sizeinfo
   for (x = 0; x <= total; x++)
     {
+
+      if (DEBUG)
+	{
+	  printf ("%.2x ", data[x]);
+	  if ((x % 22) == 0 && x != 0)
+	    printf ("\n");
+
+	}
       if (data[x] == 0x55 && data[x + 1] == 0x50 && data[x + 2] == 0x58
 	  && data[x + 3] == 0x21)
 	{
@@ -73,7 +80,8 @@ data = malloc(stats.st_size * sizeof(char));
 
 	    }
 
-          if (head == 2) printf("\n");
+	  if (head == 2)
+	    printf ("\n");
 
 	  if (head == 3)
 	    {
@@ -87,6 +95,17 @@ data = malloc(stats.st_size * sizeof(char));
 		  printf ("0x%.2x ", data[(x + 24) + z]);
 		}
 	      printf ("\n");
+	    }
+	  if (head == 4)
+	    {
+	      printf ("\nFound 4th UPX! Header, using p_filesize :");
+
+	      size = x + 24;
+	      for (z = 0; z < 4; z++)
+		{
+		  printf ("0x%.2x ", data[(x + 24) + z]);
+		}
+	      printf (" Instead. \n");
 	    }
 	}
     }
@@ -110,26 +129,28 @@ data = malloc(stats.st_size * sizeof(char));
     }
 
   snprintf (filename, 249, "%s.fixed", argv[1]);
-  fout = open (filename, O_WRONLY | O_CREAT |O_TRUNC,0644);
+  fout = open (filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-  if (!fout) {
-      fprintf(stderr, "Error %s %s\n", filename, strerror( errno ));
+  if (!fout)
+    {
+      fprintf (stderr, "Error %s %s\n", filename, strerror (errno));
       exit (0);
-  }
+    }
 
   printf ("\nTotal bytes read %d\n\nWriting file %s\n", total, filename);
-  total=0;
+  total = 0;
   while (total < stats.st_size)
     {
       write (fout, &data[total], 1);
-      float percent=0.0, t=total,s=stats.st_size;
-      percent = (t/s) * 100;
-      if (total % 10000 == 0) printf("%.2f %%\n",percent);
+      float percent = 0.0, t = total, s = stats.st_size;
+      percent = (t / s) * 100;
+      if (total % 10000 == 0)
+	printf ("%.2f %%\n", percent);
       total++;
     }
-    printf("Done\n");
+  printf ("Done\n");
   close (fout);
-  free(data);
+  free (data);
   return 0;
 }
 
@@ -140,4 +161,17 @@ print_usage (char *arg)
   printf ("To attempt to repair a corruptted UPX packed malware sample.\n\n");
   printf ("Usage: %s filename\n", arg);
   exit (0);
+}
+
+
+
+void print_banner(char *arg) {
+
+printf("+=====================================================================================+\n");
+printf("|                       UPX! Corrupt Header Fixer v1.0                                |\n");
+printf("|                                                                                     |\n");
+printf("|                       Larry W. Cashdollar, 4/20/2021                                |\n");
+printf("|                                                                                     |\n");
+printf("+=====================================================================================+\n");
+printf("Reading File :%s\n",arg);
 }
