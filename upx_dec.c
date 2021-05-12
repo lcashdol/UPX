60365254c64488adb33ffe58e81ad8bd2a421c50
Larry W. Cashdollar <lcashdol@gmail.com>
2021-05-12 12:44:20 -0400

    fix valgrind warnings ø

--- upx_dec.c
+++ upx_dec.c
@@ -45,6 +45,8 @@ main (int argc, char **argv)
       fprintf (stderr, "Error %s %s\n", argv[1], strerror (errno));
       exit (0);
     }
+   //initialize array.
+   for (x=0;x<=stats.st_size;x++) data[x] = 0;
 
   fd = open (argv[1], O_RDONLY);
   if (fd < 0)
